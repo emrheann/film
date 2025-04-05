e8b8451f190be60eb42ac3140cf41366befa1e32
emrheann <emrheann@gmail.com>
2025-04-05 04:27:36 +0300

    PageProps tipi guncellendi

--- src/app/film/[id]/page.tsx
+++ src/app/film/[id]/page.tsx
@@ -5,9 +5,10 @@ interface Props {
   params: {
     id: string;
   };
+  searchParams: { [key: string]: string | string[] | undefined };
 }
 
-export default async function MoviePage({ params }: Props) {
+export default async function MoviePage({ params, searchParams }: Props) {
   const movie = await getMovieDetails(params.id);
 
   return <MoviePageClient movieId={params.id} initialMovie={movie} />;
