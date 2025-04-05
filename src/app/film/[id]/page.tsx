0f40a8c579f6832c6b93e6dca665daf35a5e8b09
emrheann <emrheann@gmail.com>
2025-04-05 03:27:12 +0300

    ESLint hataları düzeltildi

--- src/app/film/[id]/page.tsx
+++ src/app/film/[id]/page.tsx
@@ -6,10 +6,9 @@ interface MoviePageProps {
   params: {
     id: string;
   };
-  searchParams: { [key: string]: string | string[] | undefined };
 }
 
-export default async function MoviePage({ params, searchParams }: MoviePageProps) {
+export default async function MoviePage({ params }: MoviePageProps) {
   const movie = await getMovieDetails(params.id);
 
   return (
@@ -92,7 +91,7 @@ export default async function MoviePage({ params, searchParams }: MoviePageProps
                 rel="noopener noreferrer"
                 className="bg-yellow-500 text-black px-6 py-2 rounded-lg font-semibold hover:bg-yellow-600"
               >
-                IMDB'de Görüntüle
+                IMDB&apos;de Görüntüle
               </a>
               <Link 
                 href="/"
