2c26574e0baf647de4ca911d60582e470dec88d7
emrheann <emrheann@gmail.com>
2025-04-05 04:28:57 +0300

    Kullanilmayan searchParams parametresi kaldirildi

--- src/app/film/[id]/page.tsx
+++ src/app/film/[id]/page.tsx
@@ -5,10 +5,9 @@ interface Props {
   params: {
     id: string;
   };
-  searchParams: { [key: string]: string | string[] | undefined };
 }
 
-export default async function MoviePage({ params, searchParams }: Props) {
+export default async function MoviePage({ params }: Props) {
   const movie = await getMovieDetails(params.id);
 
   return <MoviePageClient movieId={params.id} initialMovie={movie} />;
