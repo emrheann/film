f9eb38ad6d32ac49c0d1477f48a37d11199dd8fd
emrheann <emrheann@gmail.com>
2025-04-05 04:26:26 +0300

    Film detay sayfasi server component olarak guncellendi

--- src/app/film/[id]/page.tsx
+++ src/app/film/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import { getMovieDetails } from "@/services/omdb";
 import MoviePageClient from "./MoviePageClient";
 
 interface Props {
@@ -7,17 +7,8 @@ interface Props {
   };
 }
 
-export default function MoviePage({ params }: Props) {
-  return (
-    <Suspense fallback={
-      <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
-        <div className="text-center">
-          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-yellow-500 mx-auto"></div>
-          <p className="mt-4 text-gray-400">Yükleniyor...</p>
-        </div>
-      </div>
-    }>
-      <MoviePageClient movieId={params.id} />
-    </Suspense>
-  );
+export default async function MoviePage({ params }: Props) {
+  const movie = await getMovieDetails(params.id);
+
+  return <MoviePageClient movieId={params.id} initialMovie={movie} />;
 } 
